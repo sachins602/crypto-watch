e0e7b95d7b0cc3b552b9dfade87eb9624059be2a
sachins602 <sachinsapkota4@gmail.com>
2022-07-30 11:40:25 +0545

    format the response from api

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/adshao/go-binance/v2"
 )
@@ -16,13 +18,50 @@ func main() {
 	//futuresClient := binance.NewFuturesClient(apiKey, secretKey)   // USDT-M Futures
 	//deliveryClient := binance.NewDeliveryClient(apiKey, secretKey) // Coin-M Futures
 
-	klines, err := client.NewKlinesService().Limit(5).Symbol("BTCUSDT").
+	klines, err := client.NewKlinesService().Limit(10).Symbol("BTCUSDT").
 		Interval("1d").Do(context.Background())
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	for _, kline := range klines {
-		fmt.Print(kline)
+		klineArrayRaw := fmt.Sprintf("%+v\n", kline)
+		formattedKlineArray := strings.ReplaceAll(
+			strings.ReplaceAll(
+				strings.ReplaceAll(klineArrayRaw, "\n", ","),
+				"&", ""),
+			" ", ", ")
+		klineArray := removeLastRune(formattedKlineArray)
+		klineArrayJson, err := json.Marshal(klineArray)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		var klineArrayJsonString Kline
+		unmarshaledKlineArray := json.Unmarshal([]byte(klineArrayJson), &klineArrayJsonString)
+
+		fmt.Println(unmarshaledKlineArray)
+
 	}
+}
+
+// Kline define kline info
+type Kline struct {
+	OpenTime                 int64  `json:"openTime"`
+	Open                     string `json:"open"`
+	High                     string `json:"high"`
+	Low                      string `json:"low"`
+	Close                    string `json:"close"`
+	Volume                   string `json:"volume"`
+	CloseTime                int64  `json:"closeTime"`
+	QuoteAssetVolume         string `json:"quoteAssetVolume"`
+	TradeNum                 int64  `json:"tradeNum"`
+	TakerBuyBaseAssetVolume  string `json:"takerBuyBaseAssetVolume"`
+	TakerBuyQuoteAssetVolume string `json:"takerBuyQuoteAssetVolume"`
+}
+
+func removeLastRune(s string) string {
+	r := []rune(s)
+	return string(r[:len(r)-1])
 }
